4f74806ddea81c4f5b40623d04f9f2e9fe9a5d2e
dil-dsingh <dsingh@diligent.com>
2024-04-14 11:34:11 +0530

    Added string sorting program

--- string/string_sorting_using_count.py
+++ string/string_sorting_using_count.py
@@ -1,16 +1,28 @@
 #Problem Description
 #Given an array A. Sort this array using Count Sort Algorithm and return the sorted array.
-class Solution:
-    # @param A : list of integers
-    # @return a list of integers
-    def solve(self, A):
-        mx = max(A)
-        freq = [0 for j in range(mx + 1)]
-        for x in A:
-            freq[x] += 1
-        ans = []
-        for i in range(mx + 1):
-            for j in range(freq[i]):
-                ans.append(i)
-        return ans
+A = 'acdgferaaaddffggzz'
+freq = [0 for j in range(26)]
+# print(freq)
+for x in A:
+    freq[ord(x)-ord('a')] += 1
+print(freq)
+ans = []
+for i in range(26):
+    for j in range(freq[i]):
+        ans.append(chr(97+i))
+print(''.join(ans))
+
+# class Solution:
+#     # @param A : list of integers
+#     # @return a list of integers
+#     def solve(self, A):
+#         mx = max(A)
+#         freq = [0 for j in range(mx + 1)]
+#         for x in A:
+#             freq[x] += 1
+#         ans = []
+#         for i in range(mx + 1):
+#             for j in range(freq[i]):
+#                 ans.append(i)
+#         return ans
 
